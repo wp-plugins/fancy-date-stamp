24ff33da39301bcba7932eac363ae03fd630d60e
markcoker <markcoker@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-16 04:06:50 +0000

    Commit version 0.2
    git-svn-id: https://plugins.svn.wordpress.org/fancy-date-stamp/trunk@698226 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Fancy Date Stamp.php
@@ -0,0 +1,166 @@
+<?php
+/**
+ * @package Fancy Date Stamp
+ * @version 0.2
+ */
+/*
+Plugin Name: Fancy Date Stamp
+Description: This provides you with a nice way of displaying the date in your posts. This took a whole day *sigh* amateur hour.
+Version: 0.2
+Author: Mark Coker
+License: None (Open Domain)
+*/
+add_action( 'init', 'register_shortcodes');
+
+function register_shortcodes(){
+	add_shortcode('date-stamp', 'insert_date_stamp');
+}
+
+function stylesheet(){
+$style="<style type='text/css'>
+	@import url(http://fonts.googleapis.com/css?family=Amaranth);
+
+	.entry-title {
+		display: none;
+		padding-top: 0;
+	}
+	.entry-meta {
+		display: none;
+	}
+	.post-header{
+		clear: both;
+		float: left;
+		margin-left: 0;
+		width: 100%;
+		display: block;
+	}
+	.stamp-container {
+		clear: both;
+		float: left;
+		margin-left: 0;
+		width: auto;
+		display: block;
+
+		background: transparent url('".	plugins_url( 'assets/strips.jpg' , __FILE__ )."') 100% 0 repeat-y;
+		width: 25px;
+		padding-right:15px;
+	}
+	.day {	
+		font-size: 26px;
+		font-family: 'Amaranth', sans-serif;
+		color: #DD1870;
+		line-height: 18px;
+		margin-top: 3px;
+	}
+
+	.month {
+		color: #000;
+		display: block;
+		font-size: 13px;
+		font-family: 'Amaranth', sans-serif;
+	}
+	.post-title {
+		position: relative;
+		left:10px;
+		font-family: 'Amaranth', sans-serif;	
+		color: #444766;
+		font-size:24px;
+		line-height: 24px;
+	}
+	.categories {
+		position: relative;
+		left:10px;
+		color: #000;
+		font-size: 10px;
+		line-height: 9px;
+		font-family: 'Amaranth', sans-serif;
+		text-transform: uppercase;
+	}	
+
+	.categories a{	
+		color: #DD1870;
+	}
+	.categories a:hover, a:active {
+		color: #000;
+	}
+
+	</style>";
+return $style;
+}
+
+function getmonth(){
+	$mon = date("m");
+	switch ($mon){
+		case 1:
+			$month = "JAN";
+			break;
+		case 2:
+			$month = "FEB";
+			break;
+		case 3:
+			$month = "MAR";
+			break;
+			case 4:
+			$month = "APR";
+			break;
+		case 5:
+			$month = "MAY";
+			break;
+		case 6:
+			$month = "JUN";
+			break;	
+		case 7:
+			$month = "JUL";
+			break;	
+		case 8:
+			$month = "AUG";
+			break;	
+		case 9:
+			$month = "SEP";
+			break;	
+		case 10:
+			$month = "OCT";
+			break;	
+		case 11:
+			$month = "NOV";
+			break;	
+		case 12:
+			$month = "DEC";
+			break;	
+		default:
+	        $month = "error!";
+	}
+return $month;
+}
+
+function categories(){
+	$categories = get_the_category();
+	$spacer= ' ';
+	$list= '';
+	if($categories){
+		foreach($categories as $category) {
+			$list .= '<a class"categories" href="'.get_category_link( $category->term_id ).'" title="' . esc_attr( sprintf( __( "View all posts in %s" ), $category->name ) ) . '">'.$category->cat_name.'</a>'.$spacer;
+		}
+	$categories_output = trim($list);
+	}
+return $categories_output;
+}
+
+function insert_date_stamp() {
+	
+	$tday = date("d");
+	$return_string .= "".stylesheet()."
+		<div class='post-header'>
+		<div class='stamp-container'>
+			<span class = 'day'>".$tday."</span>
+			<span class ='month'>".getmonth()."</span></div>
+		<div class = 'post-title'>".get_the_title()."</div>
+			<small class = 'categories'>categories: ".categories()."</small>
+		</div>";
+	 
+	return $return_string;
+
+}
+	
+
+?>
---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== Plugin Name ===
+Contributors: markcoker
+Tags: Fancy Date Stamp, DateStamp, Date, Pretty, Fancy, Stamp, Fun, Exciting, Blog, Automatic, Free
+Requires at least: 3.0.1
+Tested up to: 3.5.1
+Stable tag: trunk
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This provides you with a nice way of displaying the date in your posts. This took a whole day *sigh* amateur hour.
+
+== Description ==
+
+I made this for a site i was making for a friend, thought others might like to use it. The way its loaded is not the most efficient but feel free to change it!
+
+I tested this on the Twenty Eleven and it seemed to be ok. (needs more testing though)
+
+This concludes my first plugin description :).
+
+Enjoy!!!
+
+== Installation ==
+
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Screenshots ==
+
+1. This is how it looks!
+
+== Changelog ==
+
+= 0.1 =
+Let it Begin!
+= 0.2 =
+Local CSS & Image Reference
